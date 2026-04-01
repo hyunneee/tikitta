77e21395f80d5906c45c4ad5d2b34718c5598f14
hyunneee <82044319+hyunneee@users.noreply.github.com>
2026-04-01 23:23:01 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/tikitta/",
   plugins: [react(), mkcert()],
   server: {
     https: true,
@@ -11,7 +12,7 @@ export default defineConfig({
     proxy: {
       // /api로 시작하는 요청은 백엔드로 전달
       "/api": {
-        target: "https://back-tikitta.duckdns.org", // 실제 백엔드 주소
+        target: "", // 실제 백엔드 주소
         changeOrigin: true, // CORS 우회
         rewrite: (path) => path.replace(/^\/api/, ""), // /api 제거 후 전달
       },
